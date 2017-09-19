57c07227a523af34251bda2c7508cc24a2ae50ec
Christian Arredondo <carredondo@lja.com>
2017-09-19 17:50:36 -0500

    implement complete addNote function

--- notes.js
+++ notes.js
@@ -1,20 +1,41 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 const addNote = (title, body) => {
-  console.log('Adding note:', title, body);
-}
+  let notes = [];
+  let note = {
+    title,
+    body
+  };
+
+  try {
+    let notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (e) {
+  
+  }
+
+  let duplicates = notes.filter((note) => note.title === title);
+
+  if (!duplicates.length) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
+
+};
 
 const getAll = () => {
   console.log('Getting all notes...');
-}
+};
 
 const readNote = (title) => {
   console.log('Reading note:', title);
-}
+};
 
 const removeNote = (title) => {
   console.log('Removing note:', title);
-}
+};
 
 
 module.exports = {
