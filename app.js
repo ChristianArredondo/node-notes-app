351f1a18fd0b8a1c59591c2ddfac6885f4c6eba3
Christian Arredondo <carredondo@lja.com>
2017-09-18 19:05:24 -0500

    create add function in notes.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js')
 
-let user = os.userInfo();
-console.log(user);
+let res = notes.add(1, 3);
+console.log('Result:', res);
 
-fs.appendFile('greetings.txt', `Hello ${ user.username } !`, function (err) {
-  if (err) {
-    console.log('Unable to write file.');
-  }
-});
+// let user = os.userInfo();
+
+// fs.appendFile('greetings.txt', `Hello ${ user.username }! You are ${ notes.age }.`, function (err) {
+//   if (err) {
+//     console.log('Unable to write file.');
+//   }
+// });
