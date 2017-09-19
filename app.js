a2a862e2080d582ca46f8ef851a95129baff946c
christianarredondo <christianarredondo@utexas.edu>
2017-09-18 22:20:58 -0500

    setup crud functionality using yargs

--- app.js
+++ app.js
@@ -2,5 +2,27 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js')
+
+
+const argv = yargs.argv;
+const command = argv._[0];
+console.log(argv);
+console.log(command);
+
+if (command === 'add') {
+  console.log('Adding new note...');
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command === 'read') {
+  console.log('Reading note...');
+  notes.readNote(argv.title);
+} else if (command === 'remove') {
+  console.log('Removing note...');
+  notes.removeNote(argv.title);
+} else {
+  console.log('Command not found...')
+}
